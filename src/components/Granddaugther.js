d52257b69fff3f315d0c04ad6f29a4b2e75b2271
Jorge-William <jorge_william_cardoso@yahoo.com.br>
2021-06-28 21:29:06 -0300

    Setting the Consumer

--- src/components/Granddaugther.js
+++ src/components/Granddaugther.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import MyContext from './MyContext';
 class Granddaugther extends React.Component {
 
   render() {
@@ -8,8 +8,18 @@ class Granddaugther extends React.Component {
         <h4>
           Hello i'm Granddaugther
         </h4>
-        <p>{`I have been received ${this.props.money} to spend.`}</p>
-        <button onClick={this.props.spendMoney}>Spend Money!!!</button>
+        <MyContext.Consumer>
+          {
+            value => (
+              <div>
+                <p>{value.money}</p>
+                <button onClick={value.spendMoney}>Spend money</button>
+              </div>
+            )
+          }
+        </MyContext.Consumer>
+        {/* <p>{`I have been received ${this.props.money} to spend.`}</p>
+        <button onClick={this.props.spendMoney}>Spend Money!!!</button> */}
       </div >
     )
   }
